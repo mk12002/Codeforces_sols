6cd20d2d6beb7a4dc39382c38a031623afe3074f
Mohit Kumar <mkd1112002@gmail.com>
2023-11-15 09:12:09 +0530

    2nd commit

--- Treasure Chest.cpp
+++ Treasure Chest.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main(){
-    
+    int tc;
+    cin>>tc;
+    while (tc--){
+        int x,y,k;
+        cin>>x>>y>>k;
+
+        int time_to_key = abs(x - y);
+        int smallest_time = time_to_key + min(time_to_key, k);
+        cout << smallest_time << endl;
+    }
 
     return 0;
 }
